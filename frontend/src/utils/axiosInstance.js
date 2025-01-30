15bb5cf3e240a8dd8161ed477733b481e6c8bb72
Yuhwan Kim <yhkim8046@gmail.com>
2025-01-30 12:11:13 +0900

    fix: api address and cors updates

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080', // Express 서버 URL
+  baseURL: 'http://13.124.78.206:8080', // Express 서버 URL
   withCredentials: true, // 쿠키를 포함하기 위한 설정
   timeout: 5000, // 요청 제한 시간 (ms)
   headers: {
